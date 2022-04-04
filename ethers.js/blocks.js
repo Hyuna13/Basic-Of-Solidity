b1506d2e6247c9834ebf3a459a871568bece5e61
Hyuna13 <75462777+Hyuna13@users.noreply.github.com>
2022-04-04 21:48:42 +0900

    Create blocks.js

---
+++ ethers.js/blocks.js
@@ -0,0 +1,18 @@
+const { ethers } = require("ethers")
+
+const INFURA_ID = ""
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+
+const main = async () => {
+    const block = await provider.getBlockNumber() //recent block
+    console.log(`\nBlock Number: ${block}\n`)
+
+    const blockInfo = await provider.getBlock(block)
+    console.log(blockInfo)
+
+    const { transactions } = await provider.getBlockWithTransactions(block) //get transaction
+    console.log(`\nLogging first transaction in block\n`)
+    console.log(transactions[0])
+}
+
+main()
